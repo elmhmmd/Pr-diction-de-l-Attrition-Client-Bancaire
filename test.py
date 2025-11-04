263906b9042c0424c5a0396776f3fffa36e81a7d
Mohammed <youcodemohammed@gmail.com>
2025-11-04 16:53:55 +0100

    ACB-14

--- test.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = (SparkSession.builder
-         .appName('Prediction_Attrition_Client')
-         .master('local[*]')
-         .getOrCreate())
-
-print(spark.version)
